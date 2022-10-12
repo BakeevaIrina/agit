d226befd4baf3ff2b5561db8dfcdf5fa61225eea
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-12 14:24:22 +0700

    Сумма двух рандомных чисел

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10);//Задаем рандомное число от 1, 2, 3, ..., 9
+Console.WriteLine(numberA);//Показываем число в терминале в виде Line
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA + numberB); 
