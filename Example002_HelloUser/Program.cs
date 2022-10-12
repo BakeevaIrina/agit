1673d0e8b839eb1f980f41d1bf5e1c78b99910a8
Bakeeva Irina <bakeeva.irina.a@gmail.com>
2022-10-12 14:00:44 +0700

    Привет user

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите ваше имя ");
+String username = Console.ReadLine();//строка с переменной
+Console.Write("Привет, ");//вывод текста
+Console.Write(username);//добавить имя к тексту
+
